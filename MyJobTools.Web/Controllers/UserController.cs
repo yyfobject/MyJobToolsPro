5df4f9159daf95db2d3b470c74c070d7c842e02f
yyfobject <yyfobject@163.com>
2018-01-30 14:19:20 +0800

    bug

--- MyJobTools.Web/Controllers/UserController.cs
+++ MyJobTools.Web/Controllers/UserController.cs
@@ -20,7 +20,7 @@ namespace MyJobTools.Web.Controllers
             //list.Add(new UserModel { UserCode = "444", UserName = "4444" });
             //list.Add(new UserModel { UserCode = "555", UserName = "5555" });
             //list.Add(new UserModel { UserCode = "66", UserName = "6666" });
-            //UserDao dao = new UserDao();
+            UserDao dao = new UserDao();
             var resultlist = dao.GetPage(queryModel, pageModel);
             //if (resultlist == null || resultlist.Count == 0)
             //{
