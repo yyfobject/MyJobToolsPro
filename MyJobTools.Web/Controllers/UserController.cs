3b62a9c03f7f4971caf9b8d09f0a253839d332b9
yyfobject <yyfobject@163.com>
2018-01-30 14:17:49 +0800

    去掉测试数据

--- MyJobTools.Web/Controllers/UserController.cs
+++ MyJobTools.Web/Controllers/UserController.cs
@@ -13,23 +13,23 @@ namespace MyJobTools.Web.Controllers
         public ActionResult List(UserQueryModel queryModel, PageQueryModel pageModel)
         {
 
-            var list = new List<UserModel>();
-            list.Add(new UserModel { UserCode = "111", UserName = "1111" });
-            list.Add(new UserModel { UserCode = "222", UserName = "2222" });
-            list.Add(new UserModel { UserCode = "333", UserName = "3333" });
-            list.Add(new UserModel { UserCode = "444", UserName = "4444" });
-            list.Add(new UserModel { UserCode = "555", UserName = "5555" });
-            list.Add(new UserModel { UserCode = "66", UserName = "6666" });
-            UserDao dao = new UserDao();
+            //var list = new List<UserModel>();
+            //list.Add(new UserModel { UserCode = "111", UserName = "1111" });
+            //list.Add(new UserModel { UserCode = "222", UserName = "2222" });
+            //list.Add(new UserModel { UserCode = "333", UserName = "3333" });
+            //list.Add(new UserModel { UserCode = "444", UserName = "4444" });
+            //list.Add(new UserModel { UserCode = "555", UserName = "5555" });
+            //list.Add(new UserModel { UserCode = "66", UserName = "6666" });
+            //UserDao dao = new UserDao();
             var resultlist = dao.GetPage(queryModel, pageModel);
-            if (resultlist == null || resultlist.Count == 0)
-            {
-                foreach (var item in list)
-                {
-                    dao.Insert(item);
-                }
-                resultlist = list;
-            }
+            //if (resultlist == null || resultlist.Count == 0)
+            //{
+            //    foreach (var item in list)
+            //    {
+            //        dao.Insert(item);
+            //    }
+            //    resultlist = list;
+            //}
             ViewBag.list = resultlist;
 
             return View();
